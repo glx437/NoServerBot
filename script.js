20ed041c25dea98d44b099ddb36dc36f0022eaf9
glx437 <glx437accou1@gmail.com>
2025-09-10 15:11:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+let lastRespondedMessageId = 0
+
 function handleUpdate(update, sendMessage) {
     if (update.message && update.message.from && !update.message.from.is_bot) {
         const chatId = update.message.chat.id
+        const messageId = update.message.message_id
         const text = update.message.text
-        if (text && text.toLowerCase() === "hello") sendMessage(chatId, "Hello!")
+
+        if (messageId <= lastRespondedMessageId) return
+
+        lastRespondedMessageId = messageId
+
+        if (text && text.toLowerCase() === "hello") {
+            sendMessage(chatId, "Hello!")
+        }
     }
 }

afabbf6fbc826b73110caa9e392a30ea5ea96dfb
glx437 <glx437accou1@gmail.com>
2025-09-10 14:43:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+function handleUpdate(update, sendMessage) {
+    if (update.message && update.message.from && !update.message.from.is_bot) {
+        const chatId = update.message.chat.id
+        const text = update.message.text
+        if (text && text.toLowerCase() === "hello") sendMessage(chatId, "Hello!")
+    }
+}

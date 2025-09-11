4e3fbf3c61e70e00ad7d72d6de6a2e73fc8bbc4f
glx437 <glx437accou1@gmail.com>
2025-09-11 12:10:33 +0300

    Update example.js

--- example.js
+++ example.js
@@ -2,5 +2,6 @@ window.handleUpdate = async function(message, BotInstance) {
     if (!message || !message.from || message.from.is_bot) return
     const chatId = message.chat.id
     const file = BotInstance.type(message)
+    if (!file) return
     await BotInstance.send(file, chatId)
 }
